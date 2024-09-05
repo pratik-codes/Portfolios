89069647ae2f55cb08ac047d263df5407e6a9bbf
pratik-codes <pratik2018id@gmail.com>
2024-09-06 01:43:13 +0530

    add new data on homescreen

--- components/section.tsx
+++ components/section.tsx
@@ -1,28 +1,51 @@
+import { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
+import { PersonalProjectData, ProjectData } from "@/utils/data"
+import AvatarPictuer from "assests/avatar-pic.jpg"
+import ProfilePicture from "assests/profile-pic.jpg"
 
 import { Icons } from "@/components/icons"
+
+import { Badge } from "./ui/badge"
 
 export const IntroSection = () => {
   return (
     <>
-      <Image
-        style={{ marginLeft: "-5px" }}
-        className="rounded-full mb-6 "
-        height={200}
-        width={200}
-        src="https://pbs.twimg.com/profile_images/1663538735093514240/ffTqlIAb_400x400.jpg"
-        alt="profile_picture"
-      />
+      <div className="relative group w-[200px] h-[200px] mb-8">
+        <div className="absolute w-full h-full rounded-full ring-4 ring-emerald-500 transition-opacity duration-300 group-hover:ring-0">
+          <Image
+            src={AvatarPictuer}
+            className="rounded-full object-cover"
+            layout="fill" // Ensures the image fills the container while maintaining aspect ratio
+            alt="avatar_picture"
+          />
+        </div>
+        <Image
+          src={ProfilePicture}
+          className="absolute rounded-full transition-opacity duration-300 opacity-0 group-hover:opacity-100 object-cover"
+          layout="fill" // Ensures the image fills the container while maintaining aspect ratio
+          alt="profile_picture"
+        />
+      </div>
       <div className="text-3xl text-white font-extrabold">
-        Hey, I'm Pratik <span className="animate-shake"><span className="animate-pulse">
-          👋
-        </span>
+        Hey, I'm Pratik{" "}
+        <span className="animate-shake">
+          <span className="animate-pulse">👋</span>
         </span>
       </div>
+
       <div className="text-gray-500 mt-2  text-md">
         I'm a <>fullstack developer 🚀</>, <>optimist 🧠</>, and a{" "}
         <>open source advocate 🥑</>. I'm currently working on my OS app called{" "}
+        <Link
+          className="text-white hover:underline hover:font-medium"
+          href="https://sifusecurity.com"
+          target="_blank"
+        >
+          sifusecurity.com
+        </Link>{" "}
+        and{" "}
         <Link
           className="text-white hover:underline hover:font-medium"
           href="https://dashlit.com"
@@ -49,25 +72,39 @@ export const ProjectSection = () => {
     <>
       {" "}
       <div className="mt-12">
-        <div className="text-2xl text-white  font-bold ">Projects.</div>
+        <div className="text-3xl text-white font-bold">Projects.</div>
         <div className="text-gray-500 mt-2 mb-2  text-md">
           things I have created
         </div>
-
-        <Link
-          href="https://dashlit.com/"
-          target="__blank"
-          className="hover:underline hover:bg-transparent hover:font-medium transition-all"
-        >
-          <div className="text-white text-md mt-6 underline font-medium"></div>
-          <div className="text-white text-md mt-6 underline font-medium">
-            Dashlit
-          </div>
-          <div className="text-gray-500  text-md hover:none">
-            An open source browser extension to boost your productivity and
-            browser experience.
-          </div>
-        </Link>
+        {PersonalProjectData.map((project: ProjectData) => {
+          return (
+            <div className="mt-8">
+              <Link
+                href={project.titleLink}
+                target="_blank"
+                className="transition-all hover:underline hover:bg-transparent hover:font-medium mb-4j"
+              >
+                <div className="mt-6 text-md font-medium text-white underline">
+                  {project.title}
+                </div>
+              </Link>
+              <div className="text-md text-gray-500 space-y-2">
+                {/* // map over description and show it */}
+                {project.description.map((description: string) => {
+                  return (
+                    <div className="text-md text-gray-500">{description}</div>
+                  )
+                })}
+              </div>
+              {/* // show all the hastags */}
+              <div className="text-md text-gray-500 mt-1">
+                {project.hastags.map((tag: string) => {
+                  return <Badge variant="outline">#{tag}</Badge>
+                })}
+              </div>
+            </div>
+          )
+        })}
 
         <div className="text-white   text-md mt-6 underline font-medium">
           Projects I have built working with companies
--- utils/data.ts
+++ utils/data.ts
@@ -32,7 +32,8 @@ export const BlogData: any = [
     description:
       "Sharing about monorepos and how to use them in your projects and thier pros and cons.",
     url: "https://blogs.tiwaripratik.com/mono-respositories-in-jsts-what-why-and-how-with-nx",
-    imageSrc: "https://cdn.hashnode.com/res/hashnode/image/upload/v1674841183755/4f03691a-2140-4560-85c6-ce01c53096de.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    imageSrc:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1674841183755/4f03691a-2140-4560-85c6-ce01c53096de.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
     hastags: ["monorepos", "nx", "javascript", "typescript"],
   },
   {
@@ -40,7 +41,8 @@ export const BlogData: any = [
       "Understanding React's UI Rendering Process (understanding virtual dom in depth)",
     description: "Sharing about how react works under the hood.",
     url: "https://blogs.tiwaripratik.com/understanding-reacts-ui-rendering-process-understanding-virtual-dom-in-depth",
-    imageSrc: "https://cdn.hashnode.com/res/hashnode/image/upload/v1671198938640/WNU1O94w5.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    imageSrc:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1671198938640/WNU1O94w5.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
     hastags: ["react", "virtual-dom", "javascript"],
   },
   {
@@ -49,7 +51,8 @@ export const BlogData: any = [
     description:
       "Undertanding nestjs framework and architecture. Also a open source low code tool called Amplification that lets us write nestjs services very easily.",
     url: "https://blogs.tiwaripratik.com/dive-into-nestjs-and-amplication-a-powerful-framework-for-nodejs-and-an-easy-way-to-build-your-services",
-    imageSrc: "https://cdn.hashnode.com/res/hashnode/image/upload/v1687011196292/72738b0d-4778-449e-90b7-d6d2b6317a82.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    imageSrc:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1687011196292/72738b0d-4778-449e-90b7-d6d2b6317a82.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
     hastags: ["nestjs", "javascript", "typescript"],
   },
   {
@@ -58,7 +61,8 @@ export const BlogData: any = [
     description:
       "understanding how google auth works with chrome extensions running with firebase on manifest json version 3.",
     url: "https://blogs.tiwaripratik.com/simplifying-google-authentication-with-mv3-and-firebase-for-chrome-extensions-reacttypescript",
-    imageSrc: "https://i.pcmag.com/imagery/articles/027qzlp4GUvpopUjd4UTTRF-11.fit_lim.size_1600x900.v1664814287.jpg",
+    imageSrc:
+      "https://i.pcmag.com/imagery/articles/027qzlp4GUvpopUjd4UTTRF-11.fit_lim.size_1600x900.v1664814287.jpg",
     hastags: ["chrome-extensions", "firebase", "react", "typescript"],
   },
   {
@@ -66,7 +70,8 @@ export const BlogData: any = [
     description:
       "How and why is golang a better choice for building services these days.",
     url: "https://dev.to/pratikcodes/why-go-31k4",
-    imageSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--EuqhvOYk--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/fc2rbv8tuan3k1bhoh8m.png",
+    imageSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--EuqhvOYk--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/fc2rbv8tuan3k1bhoh8m.png",
     hastags: ["golang"],
   },
   {
@@ -74,7 +79,8 @@ export const BlogData: any = [
     description:
       "Some pratices that you should follow to write better javascript code. A guide to be a better javascript developer",
     url: "https://pratik-codes.medium.com/how-to-write-clean-javascript-code-9c1ea003389b",
-    imageSrc: "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
+    imageSrc:
+      "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
     hastags: ["aws", "lambda", "ec2"],
   },
   {
@@ -82,7 +88,8 @@ export const BlogData: any = [
     description:
       "Understand the internals of javascript and how code is executed in javascript",
     url: "https://pratik-codes.medium.com/how-javascript-works-execution-context-b8d30ead4666",
-    imageSrc: "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
+    imageSrc:
+      "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
     hastags: ["aws", "lambda", "ec2"],
   },
   {
@@ -90,21 +97,25 @@ export const BlogData: any = [
     description:
       "How can you upgrade/downgrade you EC2 instances with a lambda function. A particular use case I have to face in one of my projects.",
     url: "https://dev.to/pratikcodes/start-stop-upgrade-downgrade-ec2-instances-with-lambda-functions-5fn9",
-    imageSrc: "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
+    imageSrc:
+      "https://media.licdn.com/dms/image/D4D12AQEj5jjhop10Bw/article-cover_image-shrink_423_752/0/1672073941863?e=1698278400&v=beta&t=GOL7QAQtct1BwfjZKBKIbvNAerxme04n_4rgr79tovo",
     hastags: ["aws", "lambda", "ec2"],
   },
   {
     title: "Javascript concept handbook for interviews",
     description: "A resource for learning about javascript for interviews.",
     url: "https://dev.to/pratikcodes/javascript-concept-handbook-for-interviews-b0j",
-    imageSrc: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*LyZcwuLWv2FArOumCxobpA.png",
+    imageSrc:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*LyZcwuLWv2FArOumCxobpA.png",
     hastags: ["javascript", "interviews"],
   },
   {
     title: "Optimizers in Neural Networks (very brief and on point)",
-    description: "Understanding and learning about different optimizers in neural networks",
+    description:
+      "Understanding and learning about different optimizers in neural networks",
     url: "https://pratik-codes.medium.com/optimizers-in-neural-networks-in-brief-ad4b339c6acc",
-    imageSrc: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*LyZcwuLWv2FArOumCxobpA.png",
+    imageSrc:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*LyZcwuLWv2FArOumCxobpA.png",
     hastags: ["javascript", "interviews"],
   },
 ]
@@ -112,7 +123,8 @@ export const BlogData: any = [
 export const TalkData = [
   {
     place: "React Mumbai",
-    logoImage: "https://pbs.twimg.com/profile_images/1671952285730238464/SJ9ev-00_400x400.jpg",
+    logoImage:
+      "https://pbs.twimg.com/profile_images/1671952285730238464/SJ9ev-00_400x400.jpg",
     link: "/talks/generics-in-typescript",
     title:
       "Generics in Typescript. Build powerfull abstraction with typescript and say goodbye to any.",
@@ -128,16 +140,17 @@ export const TalkData = [
       "/assets/react_mumbai/REACT_MUMBAI_4.jpeg",
     ],
     twitter_embeds: [
-      '1692788188887253248',
-      '1692846506368225749',
-      '1694573356467576944',
-      '1692864425001365985',
+      "1692788188887253248",
+      "1692846506368225749",
+      "1694573356467576944",
+      "1692864425001365985",
     ],
   },
   {
     place: "React Pune",
     link: "/talks/building-frameworky-things",
-    logoImage: "https://pbs.twimg.com/profile_images/1628286577482018817/P2qjUGx5_400x400.jpg",
+    logoImage:
+      "https://pbs.twimg.com/profile_images/1628286577482018817/P2qjUGx5_400x400.jpg",
     title:
       "Building frameworky things in react from scratch. SSR, file-based routing and server-side data fetching like next.js",
     content_des:
@@ -148,9 +161,43 @@ export const TalkData = [
       "/assets/react_pune/IMG_3695.jpg",
     ],
     twitter_embeds: [
-      '1682638222206316545',
-      '1682796601025392640',
-      '1682678900621492224'
+      "1682638222206316545",
+      "1682796601025392640",
+      "1682678900621492224",
     ],
+  },
+]
+
+export interface ProjectData {
+  title: string
+  titleLink: string
+  description: string[]
+  hastags: string[]
+}
+
+export const PersonalProjectData: Array<ProjectData> = [
+  {
+    title: "Sifu Security",
+    titleLink: "https://sifusecurity.com/",
+    description: [
+      "Our app protects Solana users from malicious transactions and smart contracts. It offers a Contract Address Checker, Smart Contract Auditing, and Simulated Transaction Validator to ensure secure interactions, providing detailed insights and safety before committing to any transaction.",
+      "Won $5000+ in grants from solana foundation",
+    ],
+    hastags: [
+      "python",
+      "node",
+      "nextjs/reactjs",
+      "solana",
+      "blockchain",
+      "security",
+    ],
+  },
+  {
+    title: "Dashlit",
+    titleLink: "https://www.dashlit.com/",
+    description: [
+      "An open source browser extension to boost your productivity and browser experience.",
+    ],
+    hastags: ["reactjs", "browser-extension", "productivity"],
   },
 ]
