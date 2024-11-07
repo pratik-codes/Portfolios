4a8ccc56849d43e69ff33e34e477060636473a2e
pratik-codes <pratik2018id@gmail.com>
2024-11-08 02:59:21 +0530

    details issues fixed

--- app/talks/[id]/page.tsx
+++ app/talks/[id]/page.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { PortfolioData } from '@/app/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import { ArrowLeft } from 'lucide-react';
 
 interface TalksPageProps {
@@ -29,7 +31,7 @@ const TalkDetails: React.FC<TalksPageProps> = ({ params }) => {
                     {/* Back Button */}
                     <Link
                          href="/talks"
-                         className="mb-8 inline-flex items-center crt-text hover:text-[#008800] mb-6"
+                         className="mb-8 inline-flex items-center crt-text hover:text-[#008800]"
                     >
                          <ArrowLeft className="mr-2 h-4 w-4" />
                          <span>Back to Talks</span>
@@ -53,62 +55,65 @@ const TalkDetails: React.FC<TalksPageProps> = ({ params }) => {
                          </div>
                     </div>
 
+                    {/* Images Grid */}
+                    <div className="mb-8">
+                         <h2 className="text-xl font-bold crt-text mb-4">Event Images</h2>
+                         <div className="grid grid-cols-1 md:grid-cols-2 border border-[#00ff00]">
+                              {talk.images.map((image, index) => (
+                                   <div 
+                                        key={index} 
+                                        className="relative w-full overflow-hidden border border-[#008800]"
+                                        style={{ paddingTop: '56.25%' }} // 16:9 aspect ratio
+                                   >
+                                        <Image
+                                             src={image}
+                                             alt={`Talk image ${index + 1}`}
+                                             layout="fill"
+                                             objectFit="cover"
+                                             className="absolute top-0 left-0 w-full h-full"
+                                        />
+                                   </div>
+                              ))}
+                         </div>
+                    </div>
 
-{/* Images Grid */}
-<div className="mb-8">
-     <h2 className="text-xl font-bold crt-text mb-4">Event Images</h2>
-     <div className="grid grid-cols-1 md:grid-cols-2 border border-[#00ff00]">
-          {talk.images.map((image, index) => (
-               <div 
-                    key={index} 
-                    className="relative w-full overflow-hidden border border-[#008800] p-5"
-                    style={{ paddingTop: '56.25%' }} // 16:9 aspect ratio
-               >
-                    <Image
-                         src={image}
-                         alt={`Talk image ${index + 1}`}
-                         layout="fill"
-                         objectFit="cover"
-                         className="absolute top-0 left-0 w-full h-full"
-                    />
-               </div>
-          ))}
-     </div>
-</div>
-
-                   {/* Twitter Embeds */}
+                    {/* Twitter Embeds */}
                     {talk.twitter_embeds.length > 0 && (
                          <div className="mb-8">
                               <h2 className="text-xl font-bold crt-text mb-4">Twitter Highlights</h2>
-                         <hr className="border border-[#00ff00] mb-4" />
-                              <div className="">
+                              <hr className="border border-[#00ff00] mb-4" />
+                              <ul>
                                    {talk.twitter_embeds.map((embedId, index) => (
-                                        <div key={index}>
-                                             <li className="crt-text">https://twitter.com/i/web/status/${embedId}</li>
-                                             <Link target={"_blank"}  href={`https://twitter.com/i/web/status/${embedId}`} key={index} className="mb-4 last:mb-0 p-2">
+                                        <li key={index} className="mb-4">
+                                             <Link
+                                                  href={`https://twitter.com/i/web/status/${embedId}`}
+                                                  target="_blank"
+                                                  className="crt-text hover:text-[#008800] underline"
+                                             >
+                                                  View Tweet
                                              </Link>
-                                             </div>
+                                        </li>
                                    ))}
-                              </div>
+                              </ul>
                          </div>
                     )}
 
-
-
-                    {/* External Link
-                    <div className="border border-[#00ff00] p-4">
-                         <h2 className="text-xl font-bold crt-text mb-4">Resources</h2>
-                         <Link
-                              href={talk.link}
-                              target="_blank"
-                              className="crt-text hover:text-[#008800] underline"
-                         >
-                              View presentation slides and materials
-                         </Link>
-                    </div> */}
+                    {/* External Link */}
+                    {talk.link && (
+                         <div className="border border-[#00ff00] p-4">
+                              <h2 className="text-xl font-bold crt-text mb-4">Resources</h2>
+                              <Link
+                                   href={talk.link}
+                                   target="_blank"
+                                   className="crt-text hover:text-[#008800] underline"
+                              >
+                                   View presentation slides and materials
+                              </Link>
+                         </div>
+                    )}
                </main>
           </div>
-     )
-}
+     );
+};
 
-export default TalkDetails
+export default TalkDetails;
