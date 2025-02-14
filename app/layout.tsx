fce0d2dcf59feb36aaa8c8e3ab12475e080573fe
Pratik <pratik2018id@gmail.com>
2025-02-14 13:52:38 +0530

    fix: fixed tittle name

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Retrowave Portfolio",
-  description: "A retro terminal theme portfolio",
+  title: "Pratik",
+  description: "Pratik's personal website",
 };
 
 export default function RootLayout({
